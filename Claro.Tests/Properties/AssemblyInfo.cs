a5269847bd2d537148a93d592bdbe0c15d55e98b
Aristides R Fioretti <arfioretti@gmail.com>
2020-09-17 15:10:13 -0300

    Adicionei o projeto Test

---
+++ Claro.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Claro.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Claro.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("3577a8e7-7246-412a-be1f-79d41c6771d6")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Claro.Tests/UnitTest1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Claro.Application.Interfaces;
+using Claro.WebApi.Controllers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+using Claro.Domain.Entities;
+using System.Collections.Generic;
+using System.Runtime.Remoting.Contexts;
+
+using Moq;
+
+namespace Claro.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private ICelularAppService _celularAppService;
+
+
+        [TestInitialize]
+        public void Setup()
+        {
+            var celulares = new List<Celular>()
+            {
+                new Celular() { CelularId = 100, Model = "aaa", Price = 100, Brand = "bbb", Date = "01/01/2001", Photo = "ccc" },
+                new Celular() { CelularId = 200, Model = "ddd", Price = 200, Brand = "eee", Date = "01/01/2002", Photo = "fff" }
+            };
+
+            var repoMock = new Mock<ICelularAppService>();
+
+            repoMock.Setup(r => r.GetAll()).Returns(celulares);
+
+            _celularAppService = repoMock.Object;
+        }
+
+
+        [TestMethod]
+        public void GetAllCelulares_ShouldReturnAllCelulares()
+        {
+            // Arrange
+            var vb = new ClaroController(_celularAppService);
+
+            //Act
+            vb.Get();
+
+            ////Assert
+            Assert.IsNotNull(vb.Get());
+            Assert.AreEqual(2, vb.Get().Count());
+        }
+    }
+}
